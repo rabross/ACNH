23973006a3b0c45b93eed4f185918db70ca8f6fe
rabross <rab@rabross.com>
2020-09-01 14:06:05 +0100

    Added view binding

--- sea-creature/src/main/java/com/rabross/acnh/creature/sea/ui/SeaCreatureActivity.kt
+++ sea-creature/src/main/java/com/rabross/acnh/creature/sea/ui/SeaCreatureActivity.kt
@@ -5,9 +5,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
-import com.rabross.acnh.creature.sea.R
+import com.rabross.acnh.creature.sea.databinding.SeaCreaturesBinding
 import com.rabross.acnh.creature.sea.di.inject
-import kotlinx.android.synthetic.main.sea_creature_layout.*
 import javax.inject.Inject
 
 class SeaCreatureActivity : AppCompatActivity() {
@@ -15,14 +14,17 @@ class SeaCreatureActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private lateinit var binding: SeaCreaturesBinding
+
     private val viewModel by viewModels<SeaCreatureViewModel> { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         inject()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sea_creature_layout)
+        binding = SeaCreaturesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         viewModel.seaCreatures.observe(this) { seaCreatures ->
-            text.text = seaCreatures.toString()
+            binding.text.text = seaCreatures.toString()
         }
         viewModel.getSeaCreatures()
     }
