65809d60499a05b916754083f4ab08df1921e52f
rabross <rab@rabross.com>
2020-09-24 12:26:06 +0100

    Data binding for loading and error states

--- sea-creature/src/main/java/com/rabross/acnh/creature/sea/databinding/DataBinding.kt
+++ sea-creature/src/main/java/com/rabross/acnh/creature/sea/databinding/DataBinding.kt
@@ -1,11 +1,36 @@
 package com.rabross.acnh.creature.sea.databinding
 
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.rabross.acnh.content.creature.SeaCreatures
 import com.rabross.acnh.creature.sea.ui.SeaCreatureAdapter
+import com.rabross.acnh.creature.sea.ui.SeaCreatureViewState
 
 @BindingAdapter("update")
-internal fun RecyclerView.bindData(seaCreatures: SeaCreatures) {
-        (adapter as? SeaCreatureAdapter)?.update(seaCreatures)
+internal fun RecyclerView.bindData(seaCreaturesViewState: SeaCreatureViewState) {
+    visibility = when (seaCreaturesViewState) {
+        is SeaCreatureViewState.Loaded -> {
+            (adapter as? SeaCreatureAdapter)?.update(seaCreaturesViewState.seaCreatures)
+            VISIBLE
+        }
+        else -> GONE
+    }
+}
+
+@BindingAdapter("handleLoading")
+internal fun View.bindData(seaCreaturesViewState: SeaCreatureViewState) {
+    visibility = when (seaCreaturesViewState) {
+        is SeaCreatureViewState.Loading -> VISIBLE
+        else -> GONE
+    }
+}
+
+@BindingAdapter("handleError")
+internal fun View.bindError(seaCreaturesViewState: SeaCreatureViewState) {
+    visibility = when (seaCreaturesViewState) {
+        is SeaCreatureViewState.Error -> VISIBLE
+        else -> GONE
+    }
 }
--- sea-creature/src/main/java/com/rabross/acnh/creature/sea/ui/SeaCreatureViewModel.kt
+++ sea-creature/src/main/java/com/rabross/acnh/creature/sea/ui/SeaCreatureViewModel.kt
@@ -1,5 +1,6 @@
 package com.rabross.acnh.creature.sea.ui
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -15,26 +16,35 @@ class SeaCreatureViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
-    private val _seaCreatures = MutableLiveData<SeaCreatures>(emptyList())
-    val seaCreatures: LiveData<SeaCreatures>
+    private val _seaCreatures = MutableLiveData<SeaCreatureViewState>()
+    val seaCreatures: LiveData<SeaCreatureViewState>
         get() = _seaCreatures
 
+    @SuppressLint("CheckResult")
     fun fetchSeaCreatures() {
         seaCreaturesUseCase.execute()
             .subscribeOn(schedulers.io())
-            .doOnSuccess { seaCreatures ->
-                _seaCreatures.postValue(seaCreatures)
-            }
-            .onErrorReturn {
-                emptyList()
-            }
+            .observeOn(schedulers.ui())
             .doOnSubscribe {
+                _seaCreatures.value = SeaCreatureViewState.Loading
                 compositeDisposable.add(it)
             }
-            .subscribe()
+            .subscribe(
+                { seaCreatures ->
+                    _seaCreatures.value = SeaCreatureViewState.Loaded(seaCreatures)
+                },
+                {
+                    _seaCreatures.value = SeaCreatureViewState.Error
+                })
     }
 
     override fun onCleared() {
         compositeDisposable.clear()
     }
+}
+
+sealed class SeaCreatureViewState {
+    object Loading : SeaCreatureViewState()
+    object Error : SeaCreatureViewState()
+    data class Loaded(val seaCreatures: SeaCreatures) : SeaCreatureViewState()
 }
