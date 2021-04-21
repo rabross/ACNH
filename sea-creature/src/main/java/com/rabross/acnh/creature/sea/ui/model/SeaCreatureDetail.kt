835886c57dde12868136ad83247ed12874cb92f7
rabross <rab@rabross.com>
2021-04-21 15:01:28 +0100

    Use kotlin-parcelize for passing sea creature details

--- sea-creature/src/main/java/com/rabross/acnh/creature/sea/ui/model/SeaCreatureDetail.kt
+++ sea-creature/src/main/java/com/rabross/acnh/creature/sea/ui/model/SeaCreatureDetail.kt
@@ -1,15 +1,17 @@
 package com.rabross.acnh.creature.sea.ui.model
 
+import android.os.Parcelable
 import com.rabross.acnh.core.capitalizeWords
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
 import com.rabross.acnh.content.creature.SeaCreature as SeaCreatureEntity
 
+@Parcelize
 internal data class SeaCreatureDetail(
     val name: String = "",
     val imageUrl: String = "",
     val catchphrase: String = "",
     val speed: String = ""
-) : Serializable
+) : Parcelable
 
 internal fun SeaCreatureEntity.toSeaCreatureDetail() =
     SeaCreatureDetail(name.capitalizeWords(), imageUrl, catchphrase, speed)
